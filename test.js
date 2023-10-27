27d8ed4cc6d9e287a424793dfaeac5a64c186f6c
AdityaK538 <144983450+AdityaK538@users.noreply.github.com>
2023-10-27 14:31:01 +0530

    commit

---
+++ test.js
@@ -0,0 +1,9 @@
+function crypt(){
+    let i = 2.54;
+    let p = document.getElementById("we").value;
+    let x = p*i;
+    let z = x + " inches"
+    let q = document.getElementById("top");
+    q.innerHTML=z;
+    
+}
